c3b97eb9779d5932ab7fcf1f87263a8e057261b5
Emilio Barrera <barrera_emilio@yahoo.es>
2025-11-24 13:13:03 +0000

    Add project files.

---
+++ TipCalculatorMAUIApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TipCalculatorMAUIApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TipCalculatorMAUIApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace TipCalculatorMAUIApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BillAmountEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void Tip10Button_Clicked(object sender, EventArgs e)
+        {
+        }
+
+        private void Tip15Button_Clicked(object sender, EventArgs e)
+        {
+        }
+
+        private void Tip20Button_Clicked(object sender, EventArgs e)
+        {
+        }
+    }
+}
